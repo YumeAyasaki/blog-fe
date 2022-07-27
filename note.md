e43f92253806f30ab12a567038e6481b2bb3eb17
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-07-28 00:43:32 +0700

    Meo's homework, base page without reponsive...

---
+++ note.md
@@ -0,0 +1,6 @@
+# Note for ref:
+https://www.w3schools.io/learn/nextjs-sass-scss/
+https://www.freecodecamp.org/news/how-to-use-sass-with-css-modules-in-next-js/
+
+# Note for other things:
+Header and nav bar is not the same...
--- pages/meo-studio/1/index.js
+++ pages/meo-studio/1/index.js
@@ -1,9 +1,89 @@
 import React from 'react'
+import Image from 'next/image';
 
-function index() {
+import style from "./index.module.scss"
+
+const navBarEle = [
+  {
+    id: 1, 
+    name: "Trang chủ"
+  }, 
+  {
+    id: 2, 
+    name: "Câu chuyện"
+  },
+  {
+    id: 3, 
+    name: "Về mình"
+  },
+  {
+    id: 4, 
+    name: "Liên hệ"
+  }];
+const content = [
+  {
+    id: 1,
+    img: "/img/meo_1.png",
+    title: "Ipsum lorem",
+    context: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 2,
+    img: "/img/meo_1.png",
+    title: "Lorem ipsum",
+    context: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 3,
+    img: "/img/meo_1.png",
+    title: "Lorem ipsum",
+    context: "Lorem ipsum dolor sit amet",
+  }
+]
+
+export default function index() {
   return (
-    <div>index</div>
+    <div>
+      {/* Nav bar */}
+      <div className={style.header}>
+        <div className={style.logo}>Vườn hoa sứ</div>
+        <div className={style.navBar}>
+          {
+            navBarEle.map((ele) => 
+            <div key={ele.id} className={style.navBarEle}>{ele.name}</div>)
+          }
+        </div>
+      </div>
+      <main className={style.main}>
+        <div className={style.page}>
+          <div className={style.title}>Chào mừng</div>
+          <div className={style.content}>
+            {
+              content.map((ele) => <div key={ele.id}>
+                <div className={style.content__image}>
+                  <Image src={ele.img} alt={ele.img} layout='fill'></Image>
+                </div>
+                <div className={style.content__title}>{ele.title}</div>
+                <div className={style.content__context}>{ele.context}</div>
+              </div>)
+            }
+          </div>
+        </div>
+      </main>
+      <footer className={style.footer}>
+        <div className={style.image}>
+          <Image src="/icon/facebook.svg" alt='facebook' layout='fill' />
+        </div>
+        <div className={style.text}>MEO Studio</div>
+      </footer>
+    </div>
   )
 }
 
-export default index
+index.getLayout = function getLayout(page) {
+  return (
+    <>
+      {page}
+    </>
+  )
+}
