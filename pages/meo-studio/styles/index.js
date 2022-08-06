0f4fa9e79c963483b59e907567bba961c3e92b1e
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-08-07 00:33:09 +0700

    Prepare for exercise 3...

---
+++ pages/meo-studio/styles/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Index() {
+    const string = "No, there's nothing here..."
+    const string_2 = "There's a way to prevent this, by checking the index file in every folder. But i'm kinda lazy..."
+  return (
+    <div>
+        <div>{string}</div>
+        <div>{string_2}</div>
+    </div>
+    
+  )
+}
+
+export default Index
