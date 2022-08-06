8a109f72ff7566fdf065843836c11b1aa99de6a1
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-08-07 02:33:57 +0700

    Schema (structure... sth like that) of home page...

--- pages/meo-studio/3/index.js
+++ pages/meo-studio/3/index.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router' // Get link
 
 import style from './index.module.scss'
 
+const navElement = [
+  {
+    name: "Tiểu sử",
+    key: 1,
+    href: "/biography"
+  },
+  {
+    name: "Mục lục",
+    key: 2,
+    href: "/toc"
+  },
+  {
+    name: "Tìm kiếm",
+    key: 3,
+    href: "/"
+  },
+  {
+    name: "Liên hệ",
+    key: 4,
+    href: "/contact"
+  },
+]
+
 export default function Index() {
+  const router = useRouter();
+  const preLink = router.pathname;
+  console.log(router);
   return (
     <div>
-        <div>Nav bar</div>
+        <div className={style.navBar}>
+          <div className={style.logo}>
+            <Image src="/icon/Simple_Music.svg" alt="logo" layout='fill' objectFit='contain'/>
+          </div>
+          <div className={style.navEle}>
+            {
+              navElement.map((ele) => 
+              <Link href={preLink + ele.href} key={ele.key}>{ele.name}</Link>
+              )
+            }
+            <div>Tiểu sử</div>
+            <div>Mục lục</div>
+            <div>Tìm kiếm</div>
+            <div>Liên hệ</div>
+          </div>
+          <div className={style.logIn}>Đăng nhập</div>
+          <div className={style.signIn}>Đăng ký</div>
+        </div>
         <div>Main page</div>
     </div>
   )
